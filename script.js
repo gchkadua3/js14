291935c3501b72f4f98c2cb45ff843a03eaed2bf
gchkadua3 <120250936+gchkadua3@users.noreply.github.com>
2023-05-13 21:09:57 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function shecvla() {
+    var containerPosition = document.getElementById("container");
+    var circler = document.getElementById("circler");
+
+    containerPosition.style.alignItems = "flex-end";
+    circler.style.backgroundColor = "red";
+
+}
